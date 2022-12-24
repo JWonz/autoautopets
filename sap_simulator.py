00a9947e615952b2e51565e710a32f05f9e8c100
Joshua Wonser <joshua.wonser@gmail.com>
2022-12-24 17:55:12 -0500

    Start of building shop commandline, fetching animal list from JSON using list comprehensions.

--- sap_simulator.py
+++ sap_simulator.py
@@ -1,6 +1,83 @@
 from sap_metadata_cacher import read_most_recent_json
 
 
+
+"""
+Pipeline:
+    StartOfTurn
+    EndOfTurn
+        EndOfTurnWith3PlusGold
+        EndOfTurnWithLvl3Friend
+        EndOfTurnWith4OrLessAnimals
+    StartOfBattle
+
+    Anytime:
+        WhenDamaged
+        Hurt
+        Faint
+        Summoned
+        CastsAbility
+
+    Shop:
+        Buy
+        Sell
+        BuyAfterLoss
+        BuyTier1Animal
+        BuyFood
+        EatsShopFood
+        LevelUp
+
+    Battle:
+        BeforeAttack
+        WhenAttacking
+        AfterAttack
+        KnockOut
+"""
+
+
+def commandline_ui():
+    """
+    Starts a commandline version of the game.
+    :return:
+    """
+    sap_data = read_most_recent_json("./data")
+
+    # get list of animals based on pack used
+    animals = [p for p in sap_data["pets"].values()]
+
+
+    turn = 1
+    hearts = 10
+    trophy = 0
+    frozen_shop_slots = []
+    while hearts > 0 and trophy < 10:
+        # TODO: separate out shop/battle logic from UI specific
+        shop_cmdline(sap_data, turn)
+        battle_cmdline()
+        turn += 1
+
+
+
+def shop_cmdline(game_data, turn):
+    turn_data = data["turns"]["turn-11"] if turn > 11 else data["turns"]["turn-" + str(turn)]
+    animal_list = [p for p in game_data["pets"].values() if p["tier"] == turn_data["tiersAvailable"]]
+
+    # turn_data["animalShopSlots"]
+    # turn_data["foodShopSlots"]
+    # turn_data["tiersAvailable"]
+    # turn_data["levelUpTier"]
+
+    # Display shop pets and food
+
+
+    return
+
+
+
+def battle_cmdline():
+
+    return
+
 def run_game():
 
     start_of_turn()
@@ -58,4 +135,5 @@ def shop_round(health, wins, turn, team, frozen):
 if __name__ == '__main__':
 
     data = read_most_recent_json("./data")
+    pets = [p for p in data["pets"].values()]
     print(data)
